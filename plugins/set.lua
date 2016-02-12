fdf9ce525747febebb3c0fb0ba914a32915208e9
Mr Ah S <mrahs@emeil.ir>
2016-02-12 23:49:20 +0330

    Persian!

--- plugins/set.lua
+++ plugins/set.lua
@@ -8,24 +8,24 @@ local function save_value(msg, name, value)
   end
   if hash then
     redis:hset(hash, name, value)
-    return "Saved "..name
+    return "ذخیره شد "..name
   end
 end
 local function run(msg, matches)
   if not is_momod(msg) then
-    return "For moderators only!"
+    return "فقط برای مدیر!"
   end
   local name = string.sub(matches[1], 1, 50)
   local value = string.sub(matches[2], 1, 1000)
   local name1 = user_print_name(msg.from)
-  savelog(msg.to.id, name1.." ["..msg.from.id.."] saved ["..name.."] as > "..value )
+  savelog(msg.to.id, name1.." ["..msg.from.id.."] ذخیره شد ["..name.."] به شماره > "..value )
   local text = save_value(msg, name, value)
   return text
 end
 
 return {
   patterns = {
-   "^[!/]save ([^%s]+) (.+)$"
+   "^ذخیره ([^%s]+) (.+)$"
   }, 
   run = run 
 }
