530a7000250de29d687f1b93874234f77e53e736
Mr Ah S <mrahs@emeil.ir>
2016-02-12 23:44:11 +0330

    Persian!

--- plugins/broadcast.lua
+++ plugins/broadcast.lua
@@ -3,7 +3,7 @@ local function run(msg, matches)
 		local response = matches[3]
 		send_large_msg("chat#id"..matches[2], response)
 	end
-	if matches[1] == 'broadcast' then
+	if matches[1] == 'فرستادن' then
 		if is_sudo(msg) then -- Only sudo !
 			local data = load_data(_config.moderation.data)
 			local groups = 'groups'
@@ -18,8 +18,8 @@ local function run(msg, matches)
 end
 return {
   patterns = {
-    "^[!/](broadcast) +(.+)$",
-    "^[!/](bc) (%d+) (.*)$"
+    "^(فرستادن) +(.+)$",
+    "^(bc) (%d+) (.*)$"
   },
   run = run
 }
