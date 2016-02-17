8a0e8a437c21b80c7d8e729b5bd6e358e3e7b3df
Mr Ah S <mrahs@emeil.ir>
2016-02-17 14:07:41 +0330

    Create lock_english.lua

---
+++ plugins/lock_english.lua
@@ -0,0 +1,50 @@
+local function run(msg)
+
+    local data = load_data(_config.moderation.data)
+
+     if data[tostring(msg.to.id)]['settings']['lock_english'] == 'yes' then
+
+
+if not is_momod(msg) then
+
+
+chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+    local msg = 'You cannot use English words herer . Bye ! '
+   local receiver = msg.to.id
+    send_large_msg('chat#id'..receiver, msgads.."\n", ok_cb, false)
+
+      end
+   end
+end
+
+return {
+patterns = {
+"[Aa](.*)",
+"[Bb](.*)",
+"[Cc](.*)",
+"[Dd](.*)",
+"[Ee](.*)",
+"[Ff](.*)",
+"[Gg](.*)",
+"[Hh](.*)",
+"[Ii](.*)",
+"[Jj](.*)",
+"[Kk](.*)",
+"[Ll](.*)",
+"[Mm](.*)",
+"[Nn](.*)",
+"[Oo](.*)",
+"[Pp](.*)",
+"[Qq](.*)",
+"[Rr](.*)",
+"[Ss](.*)",
+"[Tt](.*)",
+"[Uu](.*)",
+"[Vv](.*)",
+"[Ww](.*)",
+"[Xx](.*)",
+"[Yy](.*)",
+"[Zz](.*)",
+}, 
+run = run
+}
