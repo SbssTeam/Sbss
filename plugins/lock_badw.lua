bb8ef04ddefaeb0df61eb185c62536850334aed4
Mr Ah S <mrahs@emeil.ir>
2016-02-17 14:04:21 +0330

    Update and rename antifosh.lua to lock_badw.lua

--- plugins/lock_badw.lua
+++ plugins/lock_badw.lua
@@ -5,7 +5,7 @@ local function run(msg)
     
     local data = load_data(_config.moderation.data)
     
-     if data[tostring(msg.to.id)]['settings']['antifosh'] == 'yes' then
+     if data[tostring(msg.to.id)]['settings']['lock_badw'] == 'yes' then
       
     
 if not is_momod(msg) then
